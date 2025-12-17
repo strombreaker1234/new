4065b0d0e423167036611f83fdd73d261e6121aa
pentestx <aashutosh.working@gmail.com>
2025-12-17 22:53:10 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import CTA from "@/components/CTA";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Services />
+      <CTA />
+    </>
+  );
+}
